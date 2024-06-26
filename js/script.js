525365ce4ba389bb30ae793cef7ed3d9e5e28e27
Kasa-27119 <kasa27119@gmail.com>
2024-06-26 12:16:22 +1200

    started accommodation page and initial population

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 /* jshint esversion: 6 */
 
 // accommodations array
-const accommodations = [
-  {
+const accommodations = [{
     // hostel #1
     id: 1,
     propertyName: "Kingston Hostel",
     propertyType: "hostel",
+    address: "33 Dunmore St.",
+    rating: 3,
     minPerson: 1,
     maxPerson: 1,
     minStay: 1,
@@ -15,20 +16,22 @@ const accommodations = [
     longitude: 169.14796156828785,
     latitude: -44.68833610545848,
     image: "https://images.unsplash.com/photo-1553444859-788c4b385b13?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    
+
   },
   {
     // hostel #2
     id: 2,
     propertyName: "Hedditch Cottage",
     propertyType: "hostel",
+    address: "76 Hedditch St.",
+    rating: 3,
     minPerson: 1,
     maxPerson: 1,
     minStay: 1,
     maxStay: 10,
     price: 30,
     longitude: 169.14400050370767,
-    latitude: -44.69240595726597, 
+    latitude: -44.69240595726597,
     image: "https://images.unsplash.com/photo-1535961671600-c244ea895cec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
@@ -36,21 +39,25 @@ const accommodations = [
     id: 3,
     propertyName: "Belmont Lodge",
     propertyType: "hotel",
+    address: "120 Browston St.",
+    rating: 4.5,
     minPerson: 1,
     maxPerson: 2,
     minStay: 1,
     maxStay: 5,
     price: 157,
     longitude: 169.1348656343329,
-    latitude: -44.71476276242638,  
+    latitude: -44.71476276242638,
     image: "https://images.unsplash.com/photo-1682204803376-1c7dae90db28?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    
+
   },
   {
     // hotel #2
     id: 4,
     propertyName: "Wanaka Retreat",
     propertyType: "hotel",
+    address: "65  Little St.",
+    rating: 4.5,
     minPerson: 1,
     maxPerson: 2,
     minStay: 1,
@@ -65,13 +72,15 @@ const accommodations = [
     id: 5,
     propertyName: "Lismore Motel",
     propertyType: "motel",
+    address: "10 Lismore St.",
+    rating: 3.5,
     minPerson: 2,
     maxPerson: 4,
     minStay: 3,
     maxStay: 10,
     price: 90,
     longitude: 169.12749734263406,
-    latitude: -44.68476604629336, 
+    latitude: -44.68476604629336,
     image: "https://images.unsplash.com/photo-1642552647918-32f6d5401980?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
@@ -79,13 +88,15 @@ const accommodations = [
     id: 6,
     propertyName: "Mackay Point",
     propertyType: "motel",
+    address: "59 Helwick St.",
+    rating: 3.5,
     minPerson: 2,
     maxPerson: 4,
     minStay: 3,
     maxStay: 10,
     price: 90,
     longitude: 169.13630262373874,
-    latitude: -44.7110685267468, 
+    latitude: -44.7110685267468,
     image: "https://images.unsplash.com/photo-1642552645890-69e371b0a81f?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
@@ -93,6 +104,8 @@ const accommodations = [
     id: 7,
     propertyName: "Brenner House",
     propertyType: "house",
+    address: "25 Hunter Cres.",
+    rating: 5,
     minPerson: 1,
     maxPerson: 4,
     minStay: 2,
@@ -107,6 +120,8 @@ const accommodations = [
     id: 8,
     propertyName: "Beacon Sanctuary",
     propertyType: "house",
+    address: "5 Beacon Point Rd.",
+    rating: 5,
     minPerson: 1,
     maxPerson: 4,
     minStay: 2,
@@ -120,11 +135,11 @@ const accommodations = [
 
 // Fullpage Init:
 new fullpage('#fullpage', {
-    licenseKey: 'gplv3-license',
-    autoScrolling: true,
-    scrollHorizontally: true,
-    controlArrows: true,
-    fixedElements: ".navbar",
+  licenseKey: 'gplv3-license',
+  autoScrolling: true,
+  scrollHorizontally: true,
+  controlArrows: true,
+  fixedElements: ".navbar",
 });
 
 // Prevent scroll of sections and slides:
@@ -133,35 +148,35 @@ new fullpage('#fullpage', {
 // mapbox initialisation
 
 // mapbok token
-// mapboxgl.accessToken = 'pk.eyJ1IjoiY2lhcmFuc2xvdyIsImEiOiJjbHY0ZW91YnYwOGV3MmlwOGQ5b3l3a3J3In0.EFWZEAWA13ehFAw5jdLqJA';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2lhcmFuc2xvdyIsImEiOiJjbHY0ZW91YnYwOGV3MmlwOGQ5b3l3a3J3In0.EFWZEAWA13ehFAw5jdLqJA';
 
-// const map = new mapboxgl.Map({
-//   container: "map",
-//   style: "mapbox://styles/mapbox/streets-v12",
-//   center: [169.13331678032918, -44.6950477150028],
-//   zoom: 12,
-// });
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [169.13331678032918, -44.6950477150028],
+  zoom: 12,
+});
 
 // Swiper Init:
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-  
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-// });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
 
 // jquery datepicker initialisation
-$(document).ready(function() {
+$(document).ready(function () {
   // start date field
   $("#startDate").datepicker({
     dateFormat: "dd/mm/yy"
@@ -172,3 +187,91 @@ $(document).ready(function() {
     dateFormat: "dd/mm/yy"
   });
 });
+
+// initial accommodation population
+function populateAccommodationResults() {
+
+  // get div from html
+  const accommodationResults = document.getElementById("accommodation-results");
+
+  // clear results of elements
+  accommodationResults.innerHTML = "";
+
+  // populate results div with cards for each array object
+
+  // TO FIX - ACCOMMS DETAILS DIV SHOULD BE INSIDE THE CARD
+  accommodations.forEach(accommodation => {
+    const accommodationCard = `
+    <div class="accommodation-card">
+
+      <-- swiper container -->
+      <div class="swiper">
+          <-- swiper image wrapper -->
+          <div class="swiper-wrapper">
+                  <div class="swiper-slide"><img src="${accommodation.image}" alt="${accommodation.propertyName} image 1" class="accommodation-item-image" data-id="${accommodation.id}"></div>
+                  <div class="swiper-slide"><img src="${accommodation.image}" alt="${accommodation.propertyName} image 1" class="accommodation-item-image" data-id="${accommodation.id}"></div>
+                  <div class="swiper-slide"><img src="${accommodation.image}" alt="${accommodation.propertyName} image 1" class="accommodation-item-image" data-id="${accommodation.id}"></div>
+              </div>
+                    
+              <div class="swiper-pagination"></div>
+                
+              <div class="swiper-button-prev"></div>
+              <div class="swiper-button-next"></div>
+                  
+              <div class="swiper-scrollbar"></div>
+          </div>
+      </div>
+
+      <div class="accommodation-details">
+          <-- accomm. details top row -->
+          <div class="accomm-details-top-container">
+
+              <-- accomm. detail headers container -->
+              <div class="accomm-details-header-container">
+                  <h3 class="green-header">${accommodation.propertyName}</h3>
+                  <h4 class="green-header">${accommodation.address}</h3>
+              </div>
+
+              <-- accomm. detail ratings container -->
+              <div class="accomm-details-ratings">
+                  <h3 class="accomm-ratings">${accommodation.rating}</h3>
+                  <i class="fa-solid fa-award"></i>
+              </div>
+          
+          </div>
+
+          <-- accomm. details bottom row -->
+          <div class="accomm-details-bottom-container">
+              <h3 class="black-label">$${accommodation.price}/night</h3>
+              <h3 class="black-label">${accommodation.minPerson}-${accommodation.maxPerson} guests</h3>
+          </div>
+      </div>
+    </div>
+    `;
+
+    // append accommodation card to results grid
+    accommodationResults.innerHTML += accommodationCard;
+  });
+
+  // re-initialise swiper
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+}
+
+// initial population callback
+populateAccommodationResults();
